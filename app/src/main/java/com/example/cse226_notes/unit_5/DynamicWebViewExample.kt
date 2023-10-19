5f7dc01b4bfae4bcec159ea302acadc3a01fef5a
Saksham Gupta <saksham.gupta002@gmail.com>
2023-10-19 12:30:52 +0530

    Static and dynamic web view

---
+++ app/src/main/java/com/example/cse226_notes/unit_5/DynamicWebViewExample.kt
@@ -0,0 +1,12 @@
+package com.example.cse226_notes.unit_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cse226_notes.R
+
+class DynamicWebViewExample : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dynamic_web_view_example)
+    }
+}
---
+++ app/src/main/java/com/example/cse226_notes/unit_5/StaticWebViewExample.kt
@@ -0,0 +1,31 @@
+package com.example.cse226_notes.unit_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import com.example.cse226_notes.R
+
+class WebViewExample : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view_example)
+
+        webView = findViewById(R.id.webView)
+
+        val customHtml = """
+            <html>
+                <body>
+                    <h1>Welcome to 99Acres</h1>
+                    <h2>Find your next home.</h2>
+                    <h3>Or get a land to build your own house.</h3>
+                    <p>It's a static Web HTML Content.</p>
+                </body>
+            </html>
+        """.trimIndent()
+
+        webView.loadData(customHtml, "text/html", "UTF-8")
+    }
+}
